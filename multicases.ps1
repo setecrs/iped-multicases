67c9beabfe9c9cfc44526cdcd01c1dc91d596acf
Aristeu Jr <aristeu.jr@gmail.com>
2023-09-28 10:54:13 -0300

    IPED 4 adjustments

--- multicases.ps1
+++ multicases.ps1
@@ -1,12 +1,12 @@
 write-host "*****************************************************************"
-write-host "Script para criação de multicasos na apreensão"
+write-host "Script para criaï¿½ï¿½o de multicasos na apreensï¿½o"
 write-host "*****************************************************************"
 
 if ($args.count -gt 1 ){
-   write-host "Script não suporta multiplos argumentos. Saindo..."
+   write-host "Este script nÃ£o suporta multiplos argumentos. Saindo..."
    pause
-   exit 
-} 
+   exit
+}
 
 # Search for folder "indexador"
 # Returns a path to iped-search-app.jar
@@ -18,7 +18,7 @@ function FindJar ($path, $depth)
     $subdirs = @(Get-ChildItem -Path $path -Dir -ErrorAction SilentlyContinue)
     $names = $subdirs | % {$_.Name}
     if ("indexador" -in $names){
-        return "$($path)\indexador\lib\iped-search-app.jar"
+        return "$($path)\iped\lib\iped-search-app.jar"
     }
     foreach($s in $subdirs) {
         $f = FindJar $s.FullName $depth-1
@@ -38,7 +38,7 @@ if ($ipedSearch){
    java -jar "$($ipedSearch)" -multicases "$($args[0])" --nologfile
 }
 else {
-   write-host "Não foi possivel encontrar arquivos IPED. Você está fazendo multicasos em uma operação ou apreensão?"
-   write-host "O script suporta multicasos no maximo em operacões. Saindo..."
+   write-host "NÃ£o foi possivel encontrar arquivos IPED. VocÃª estÃ¡ fazendo multicasos em uma operaÃ§Ã£o ou apreensÃ£o?"
+   write-host "O script suporta multicasos no mÃ¡ximo em operacÃµes. Saindo..."
 }
 pause
